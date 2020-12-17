a8fe28641e9137306f621400f609d4f488babbe7
aroldo.acevedo <aroldoacevedo@gmail.com>
2020-12-17 19:58:27 -0300

    Eliminado el GET - prueba

--- server/routes/usuario.js
+++ server/routes/usuario.js
@@ -6,10 +6,6 @@ const Usuario = require('../models/usuario');
 
 const app = express();
 
-app.get('/', function(req, res) {
-    res.json('prueba!!');
-});
-
 app.get('/usuario', function(req, res) {
     let desde = req.query.desde || 0;
     desde = Number(desde);
